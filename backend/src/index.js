a06b70e93798af73ad7012fccfa5893309b5ad59
DESKTOP-171AVQP\Admin <kienn8438@gmail.com>
2025-10-21 08:54:22 +0700

    add

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,4 +40,7 @@ app.use(express.json());
 
 const route = require('./routes/index');
 route(app);
-app.listen(5000, '0.0.0.0');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
